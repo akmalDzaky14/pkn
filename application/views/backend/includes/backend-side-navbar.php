1f96e8ac8bee0c714f8ad1b95c331071f21269fa
Akmal Dzaky <akmaldzaky33@gmail.com>
2020-02-09 14:26:49 +0700

    checkpoint 1. pembagian session

--- application/views/backend/includes/backend-side-navbar.php
+++ application/views/backend/includes/backend-side-navbar.php
@@ -28,7 +28,7 @@
     </li>
     <?php
     if (isset($_SESSION['userID'])) {
-        if (isset($_SESSION['status']) == 'admin') {
+        if ($_SESSION['status'] == 'admin') {
     ?>
             <li class="nav-item dropdown">
                 <a class="nav-link dropdown-toggle" href="#" id="pagesDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
@@ -45,32 +45,52 @@
                     <div class="dropdown-divider"></div>
                     <h6 class="dropdown-header">Other Pages:</h6>
                     <a class="dropdown-item" href="<?php echo base_url(); ?>index.php/backend/uploadProduct">Upload Product</a>
-                    <a class="dropdown-item" href="<?php echo base_url(); ?>index.php/backend/blank">Blank Page</a>
                 </div>
             </li>
-    <?php
+        <?php
         }
+        if ($_SESSION['status'] == 'admin') { ?>
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="pagesDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    <i class="fas fa-fw fa-table"></i>
+                    <span>Tables</span></a>
+                </a>
+                <div class="dropdown-menu" aria-labelledby="pagesDropdown">
+                    <h6 class="dropdown-header">List:</h6>
+                    <a class="dropdown-item" href="<?php echo base_url(); ?>index.php/backend/tables?type=Plist">Posting List</a>
+                    <a class="dropdown-item" href="<?php echo base_url(); ?>index.php/backend/tables?type=Alist">Admin List</a>
+                    <a class="dropdown-item" href="<?php echo base_url(); ?>index.php/backend/tables?type=Glist">Agent List</a>
+                    <a class="dropdown-item" href="<?php echo base_url(); ?>index.php/backend/tables?type=Ulist">User List</a>
+                    <a class="dropdown-item" href="<?php echo base_url(); ?>index.php/backend/tables?type=Klist">konfirmasi List</a>
+                    <a class="dropdown-item" href="<?php echo base_url(); ?>index.php/backend/tables?type=K2list">konfirmasi2 List</a>
+                    <a class="dropdown-item" href="<?php echo base_url(); ?>index.php/backend/tables?type=Sold">Terjual</a>
+                </div>
+            </li>
+            <li class="nav-item <?php echo $chart; ?>">
+                <a class="nav-link" href="<?php echo base_url(); ?>index.php/backend/charts?tab=chart">
+                    <i class="fas fa-fw fa-chart-area"></i>
+                    <span>Charts</span></a>;[p'3-p[0]]
+            </li>
+        <?php } elseif ($_SESSION['status'] == 'agent') { ?>
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="pagesDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    <i class="fas fa-fw fa-table"></i>
+                    <span>Tables</span></a>
+                </a>
+                <div class="dropdown-menu" aria-labelledby="pagesDropdown">
+                    <h6 class="dropdown-header">List:</h6>
+                    <a class="dropdown-item" href="<?php echo base_url(); ?>index.php/backend/tables?type=Plist">Posting List</a>
+                    <a class="dropdown-item" href="<?php echo base_url(); ?>index.php/backend/tables?type=Klist">Tersedia</a>
+                    <a class="dropdown-item" href="<?php echo base_url(); ?>index.php/backend/tables?type=K2list">Diterima</a>
+                    <a class="dropdown-item" href="<?php echo base_url(); ?>index.php/backend/tables?type=Sold">Terjual</a>
+                </div>
+            </li>
+            <li class="nav-item <?php echo $chart; ?>">
+                <a class="nav-link" href="<?php echo base_url(); ?>index.php/backend/charts?tab=chart">
+                    <i class="fas fa-fw fa-chart-area"></i>
+                    <span>Charts</span></a>
+            </li>
+    <?php }
     }
     ?>
-    <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="pagesDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-            <i class="fas fa-fw fa-table"></i>
-            <span>Tables</span></a>
-        </a>
-        <div class="dropdown-menu" aria-labelledby="pagesDropdown">
-            <h6 class="dropdown-header">List:</h6>
-            <a class="dropdown-item" href="<?php echo base_url(); ?>index.php/backend/tables?type=Plist">Posting List</a>
-            <a class="dropdown-item" href="<?php echo base_url(); ?>index.php/backend/tables?type=Alist">Admin List</a>
-            <a class="dropdown-item" href="<?php echo base_url(); ?>index.php/backend/tables?type=Glist">Agent List</a>
-            <a class="dropdown-item" href="<?php echo base_url(); ?>index.php/backend/tables?type=Ulist">User List</a>
-            <a class="dropdown-item" href="<?php echo base_url(); ?>index.php/backend/tables?type=Klist">konfirmasi List</a>
-            <a class="dropdown-item" href="<?php echo base_url(); ?>index.php/backend/tables?type=K2list">konfirmasi2 List</a>
-            <a class="dropdown-item" href="<?php echo base_url(); ?>index.php/backend/tables?type=Sold">Terjual</a>
-        </div>
-    </li>
-    <li class="nav-item <?php echo $chart; ?>">
-        <a class="nav-link" href="<?php echo base_url(); ?>index.php/backend/charts?tab=chart">
-            <i class="fas fa-fw fa-chart-area"></i>
-            <span>Charts</span></a>
-    </li>
 </ul>
